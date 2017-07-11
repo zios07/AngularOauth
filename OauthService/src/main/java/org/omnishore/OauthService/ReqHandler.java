56e12449ecd6eece8434b4800b61dd505890ae20
Zakaria KAOUKAB <zakaria.kaoukab@gmail.com>
2017-07-11 14:43:48 +0100

    Response well recieved

--- OauthService/src/main/java/org/omnishore/OauthService/ReqHandler.java
+++ OauthService/src/main/java/org/omnishore/OauthService/ReqHandler.java
@@ -1,7 +1,10 @@
 package org.omnishore.OauthService;
 
+import java.io.IOException;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -13,9 +16,21 @@ public class ReqHandler {
 	GoogleAuth helper = new GoogleAuth();
 	
 	@GET
-	@Path("/loginUrl")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String sayHi() {
+	public String getLoginUrl() {
 		return helper.buildLoginUrl();
 	}
+	
+	@GET
+	@Path("{code}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getUserInfo(@PathParam("code") String code) throws IOException {
+		
+		//Decoding the url 
+		code = code.replace("%25", "%").replace("%2F", "/");
+		String json = helper.getUserInfoJson(code);
+		System.out.println(json);
+		return json;
+	}
+	
 }
--- OauthService/src/main/webapp/angular/js/script.js
+++ OauthService/src/main/webapp/angular/js/script.js
@@ -1,11 +1,29 @@
-var app = angular.module("myApp",[]);
+var app = angular.module("myApp", [], function($locationProvider) {
+	$locationProvider.html5Mode(true);});
+	
+app.controller("authenticationCtrl", function($scope, $http, $window, $location) {
+	
+	$scope.getAuthorizationUrl = function() {
+		$http.get("http://localhost:8080/OauthService/webapi/auth/")
+				.success(function(response) {
+					$scope.loginUrl = response;
+					$window.location.href = $scope.loginUrl;
+				});
 
-app.controller("authenticationCtrl", function($scope, $http, $window){
-	$scope.goToGoogle = function(){
-		$http.get("http://localhost:8080/OauthService/webapi/auth/loginUrl").success(function(response){
-			$scope.loginUrl = response;
-			$window.location.href= $scope.loginUrl;
+	};
+	
+	$scope.code = encodeURIComponent($location.search().code).replace("%","%25");
+	
+	$scope.getUserInfo = function(){
+		$http.get("http://localhost:8080/OauthService/webapi/auth/"+$scope.code)
+		.success(function(response) {
+			$scope.profilePhoto = response.picture;
+			$scope.firstName = response.family_name;
+			$scope.lastName = response.given_name;
 		});
-		
 	}
-});
+	
+	
+})
+
+
